3296a2586df2f4cc8b8738a26ff7782f31add71c
PranayTejaReddy27 <pranayteja270@gmail.com>
2024-02-23 22:32:49 +0530

    firstMain

---
+++ ToxicCommentClassification/run.bat
@@ -0,0 +1,4 @@
+@echo off
+call .C:\Users\PRANAYTEJA\Downloads\ToxicCommentClassification\maha\Scripts\activate                                                                  
+python C:\Users\PRANAYTEJA\Downloads\ToxicCommentClassification\ToxicCommentClassification\Main.py
+pause
